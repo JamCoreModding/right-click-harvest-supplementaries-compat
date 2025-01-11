3b1af208c2330f0bb3b62d79d9e5d9b0170591fb
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-01-11 22:30:30 +0200

    Rewrite mixin entirely, it works now

--- common/src/main/java/io/github/jamalam360/rch_supplementaries_compat/mixin/RightClickHarvestMixin.java
+++ common/src/main/java/io/github/jamalam360/rch_supplementaries_compat/mixin/RightClickHarvestMixin.java
@@ -3,35 +3,52 @@ package io.github.jamalam360.rch_supplementaries_compat.mixin;
 import io.github.jamalam360.rch_supplementaries_compat.RchSupplementariesCompatPlatform;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
+import net.minecraft.world.level.block.TallGrassBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
 @Mixin(targets = "io.github.jamalam360.rightclickharvest.RightClickHarvest")
-public class RightClickHarvestMixin {
+public abstract class RightClickHarvestMixin {
 
-    @ModifyArg(
-          method = "onBlockUse",
-          at = @At(
-                value = "INVOKE",
-                target = "Lio/github/jamalam360/rightclickharvest/RightClickHarvest;onBlockUse(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;Z)Lnet/minecraft/world/InteractionResult;"
-          ),
-          index = 3
+    @Shadow
+    private static InteractionResult completeHarvest(Level level, BlockState state, BlockPos pos, Player player, InteractionHand hand, ItemStack stackInHand, boolean hoeInUse, boolean removeReplant, Runnable setBlockAction) {
+        throw new AbstractMethodError();
+    }
+
+    @Shadow
+    private static BlockState getReplantState(BlockState state) {
+        throw new AbstractMethodError();
+    }
+
+    @Redirect(
+            method = "onBlockUse",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lio/github/jamalam360/rightclickharvest/RightClickHarvest;completeHarvest(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;ZZLjava/lang/Runnable;)Lnet/minecraft/world/InteractionResult;"
+            )
     )
-    private static BlockHitResult rch_supplementaries_compat$modifyForSupplementaries(Player player, Level level, InteractionHand hand, BlockHitResult hitResult, boolean initialCall) {
-        System.out.print("rch_supplementaries_compat$modifyForSupplementaries");
-
-        BlockPos belowPos = hitResult.getBlockPos().below();
-
-        if (level.getBlockState(belowPos).is(RchSupplementariesCompatPlatform.getFlaxBlock())) {
-            return hitResult.withPosition(belowPos);
-        } else {
-            return hitResult;
+    private static InteractionResult rch_supplementaries_compat$modifyForSupplementaries(Level level, BlockState state, BlockPos pos, Player player, InteractionHand hand, ItemStack stackInHand, boolean hoeInUse, boolean removeReplant, Runnable setBlockAction) {
+        var flaxBlock = RchSupplementariesCompatPlatform.getFlaxBlock();
+        if (state.is(flaxBlock)) {
+            BlockPos belowPos = pos.below();
+            BlockState belowState = level.getBlockState(belowPos);
+            if (belowState.is(flaxBlock)) {
+                state = belowState;
+                pos = belowPos;
+                if (removeReplant)
+                    setBlockAction = () -> level.setBlockAndUpdate(belowPos, getReplantState(belowState));
+                else
+                    setBlockAction = () -> level.removeBlock(belowPos, false);
+            }
         }
+
+        return completeHarvest(level, state, pos, player, hand, stackInHand, hoeInUse, removeReplant, setBlockAction);
     }
 }
